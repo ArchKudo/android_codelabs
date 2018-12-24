ff60eb17b236786a9963cf9c598748d7d1e74ccd
ArchKudo <vgd.vyas17@gmail.com>
2018-12-24 23:55:18 +0530

    Added button to jump to top / Key takeaway: Had to cast as ScrollView to access fullScroll Method

--- app/src/main/java/local/hbar/deen/helloworld/MainActivity.java
+++ app/src/main/java/local/hbar/deen/helloworld/MainActivity.java
@@ -1,22 +1,27 @@
 package local.hbar.deen.helloworld;
 
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
-
+    private View mScrollView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mScrollView = findViewById(R.id.nested_scroll_view);
+
         // Logging
         Log.d(LOG_TAG, "Hello, Scrolling Text!");
     }
 
+    public void goUp(View view) {
+        ((ScrollView) mScrollView).fullScroll(ScrollView.FOCUS_UP);
+    }
 }
