a773e94daa702dcab3bf5e53b75c57b592efd3fb
ArchKudo <vgd.vyas17@gmail.com>
2018-12-26 00:22:16 +0530

    Re-organized imports / activity_second now has a TextField and Reply button / Bike-shed some consts / Added reference to TextViews, Buttons, EditText in both the views / setResult intent from SecondActivity to MainActivity / Receive intent from SecondActivity by using startActivityForResult and overridi ng onActivityResult method / Make MSG related TextViews in MainActivity initially invisible

--- app/src/main/java/local/hbar/deen/helloworld/MainActivity.java
+++ app/src/main/java/local/hbar/deen/helloworld/MainActivity.java
@@ -1,32 +1,50 @@
 package local.hbar.deen.helloworld;
 
 import android.content.Intent;
-import android.support.v4.widget.NestedScrollView;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int TEXT_REQUEST = 1;
+    public static final String EXTRA_SEND = "local.deen.hbar.helloworld.EXTRA.send";
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
-    private EditText mEditText;
-    public static final String EXTRA_MSG = "local.deen.hbar.helloworld.extra.MESSAGE";
+    private EditText mSend;
+    private TextView mTextHeader;
+    private TextView mTextMSG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mEditText = findViewById(R.id.editText_main);
+        mSend = findViewById(R.id.editText_main);
+        mTextHeader = findViewById(R.id.text_header);
+        mTextMSG = findViewById(R.id.text_msg);
+
         // Logging
         Log.d(LOG_TAG, "Hello, Two Activities!");
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // TODO: Check for null / Why isn't hasExtra enough?
+        if (requestCode == TEXT_REQUEST && resultCode == RESULT_OK && (data.hasExtra(SecondActivity.EXTRA_REPLY))) {
+            mTextHeader.setVisibility(View.VISIBLE);
+            mTextMSG.setVisibility(View.VISIBLE);
+            mTextMSG.setText(data.getStringExtra(SecondActivity.EXTRA_REPLY));
+        }
+    }
+
     public void launchSecondActivity(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
-        String msg = mEditText.getText().toString();
-        intent.putExtra(EXTRA_MSG, msg);
-        startActivity(intent);
+        String msg = mSend.getText().toString();
+        intent.putExtra(EXTRA_SEND, msg);
+        startActivityForResult(intent, TEXT_REQUEST);
     }
 }
--- app/src/main/java/local/hbar/deen/helloworld/SecondActivity.java
+++ app/src/main/java/local/hbar/deen/helloworld/SecondActivity.java
@@ -3,15 +3,25 @@ package local.hbar.deen.helloworld;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    public static final String EXTRA_REPLY = "local.deen.hbar.helloworld.EXTRA.reply";
+    private EditText mReply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         Intent intent = getIntent();
-        ((TextView) findViewById(R.id.text_msg)).setText(intent.getStringExtra(MainActivity.EXTRA_MSG));
+        ((TextView) findViewById(R.id.text_msg)).setText(intent.getStringExtra(MainActivity.EXTRA_SEND));
+        mReply = findViewById(R.id.reply_msg);
+    }
+
+    public void replyMSG(View view) {
+        setResult(RESULT_OK, (new Intent()).putExtra(EXTRA_REPLY, mReply.getText().toString()));
+        finish();
     }
 }
