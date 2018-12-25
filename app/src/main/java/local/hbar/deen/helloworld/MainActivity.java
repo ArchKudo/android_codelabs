ffc8f2d89fececbe41211ca6b09463be90647680
ArchKudo <vgd.vyas17@gmail.com>
2018-12-25 16:37:05 +0530

    Removed content / Changed app name for 02.1

--- app/src/main/java/local/hbar/deen/helloworld/MainActivity.java
+++ app/src/main/java/local/hbar/deen/helloworld/MainActivity.java
@@ -5,23 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
-    private View mScrollView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mScrollView = findViewById(R.id.nested_scroll_view);
-
         // Logging
         Log.d(LOG_TAG, "Hello, Scrolling Text!");
     }
 
-    public void goUp(View view) {
-        ((ScrollView) mScrollView).fullScroll(ScrollView.FOCUS_UP);
-    }
 }
