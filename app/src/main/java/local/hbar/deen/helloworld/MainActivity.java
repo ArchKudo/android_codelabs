c18d5c02ba684fc0eb6977523864ae37009c8fbd
ArchKudo <vgd.vyas17@gmail.com>
2018-12-25 22:26:38 +0530

    Send text from EditText of MainActivity SecondActivity using Bundle passed via Intent.putExtra / Received text using Intent.getStringExtra is set to SecondActivity's TextView / Added EditText and TextView to respective Activities layout

--- app/src/main/java/local/hbar/deen/helloworld/MainActivity.java
+++ app/src/main/java/local/hbar/deen/helloworld/MainActivity.java
@@ -6,20 +6,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private EditText mEditText;
+    public static final String EXTRA_MSG = "local.deen.hbar.helloworld.extra.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mEditText = findViewById(R.id.editText_main);
         // Logging
-        Log.d(LOG_TAG, "Hello, Scrolling Text!");
+        Log.d(LOG_TAG, "Hello, Two Activities!");
     }
 
     public void launchSecondActivity(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
+        String msg = mEditText.getText().toString();
+        intent.putExtra(EXTRA_MSG, msg);
         startActivity(intent);
     }
 }
