125a1680289224c3a8497b218fe522be33f4b8be
ArchKudo <vgd.vyas17@gmail.com>
2019-01-01 00:36:41 +0530

    Created a list of 3 TextViews with drawableStart images / Copied drawables to res/drawables/ Make images clickable

--- app/src/main/java/local/hbar/deen/helloworld/MainActivity.java
+++ app/src/main/java/local/hbar/deen/helloworld/MainActivity.java
@@ -1,56 +1,24 @@
 package local.hbar.deen.helloworld;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
-    private TextView coloredTextView;
-    private List<String> mColor = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        coloredTextView = findViewById(R.id.colored_txt);
-
-        Field[] fields = R.color.class.getFields();
-
-        for (Field field : fields) {
-            Log.i(LOG_TAG, field.getName());
-            mColor.add(field.getName());
-        }
-
-        if (savedInstanceState != null) {
-            coloredTextView.setTextColor(savedInstanceState.getInt("color"));
-        }
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
+    public void showDesc(View view) {
+        final Integer tag = Integer.parseInt(view.getTag().toString());
+        String[] sweets = getResources().getStringArray(R.array.sweets);
+        Toast.makeText(this, sweets[tag - 1], Toast.LENGTH_SHORT).show();
 
-        super.onSaveInstanceState(outState);
-        outState.putInt("color", coloredTextView.getCurrentTextColor());
-    }
-
-    public void changeColor(View view) {
-        String colorName = mColor.get(new Random().nextInt(mColor.size() - 1));
-        int colorResource = getResources().getIdentifier(colorName, "color", getApplicationContext().getPackageName());
-        int colorId = ContextCompat.getColor(this, colorResource);
-        coloredTextView.setTextColor(colorId);
-    }
-
-    public int g() {
-        return new Random().nextInt(255);
     }
 }
