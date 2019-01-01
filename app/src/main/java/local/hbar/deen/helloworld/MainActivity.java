36a2e4039bbc38ef65e3aaf45d1d89385a76656e
ArchKudo <vgd.vyas17@gmail.com>
2019-01-01 16:57:55 +0530

    Added onLongClickListener to the textViews / Use SparseIntArray to store OrderList

--- app/src/main/java/local/hbar/deen/helloworld/MainActivity.java
+++ app/src/main/java/local/hbar/deen/helloworld/MainActivity.java
@@ -1,24 +1,46 @@
 package local.hbar.deen.helloworld;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.SparseIntArray;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnLongClickListener {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private SparseIntArray orderList = new SparseIntArray(3);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.donut_text).setOnLongClickListener(this);
+        findViewById(R.id.froyo_text).setOnLongClickListener(this);
+        findViewById(R.id.ics_text).setOnLongClickListener(this);
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        v.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                final Integer key = Integer.parseInt(v.getTag().toString());
+                orderList.put(key, orderList.get(key, 0) + 1);
+                Log.d(LOG_TAG, orderList.toString());
+                return true;
+            }
+        });
+        return true;
     }
 
     public void showDesc(View view) {
         final Integer tag = Integer.parseInt(view.getTag().toString());
         String[] sweets = getResources().getStringArray(R.array.sweets);
-        Toast.makeText(this, sweets[tag - 1], Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Long press to add " + sweets[tag - 1], Toast.LENGTH_SHORT).show();
 
     }
+
+
 }
