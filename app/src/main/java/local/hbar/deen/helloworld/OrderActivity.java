c0b30cf96b33e0496e8a85a232a99bb14e3ca4d9
ArchKudo <vgd.vyas17@gmail.com>
2019-01-02 13:51:45 +0530

    Added couple of input methods / Landscape UI is broken in both the activities :!

--- app/src/main/java/local/hbar/deen/helloworld/OrderActivity.java
+++ app/src/main/java/local/hbar/deen/helloworld/OrderActivity.java
@@ -2,20 +2,39 @@ package local.hbar.deen.helloworld;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Locale;
 
-public class OrderActivity extends Activity {
+public class OrderActivity extends Activity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
         int[] orderCount = getIntent().getIntArrayExtra("orderCount");
-        ((TextView) findViewById(R.id.order_txt)).setText(String.format(Locale.getDefault(), "Donuts: %d\nFroyo: %d\nIce-cream Sandwich: %d\n",
+        ((TextView) findViewById(R.id.content_text)).setText(String.format(Locale.getDefault(), "Donuts: %d\nFroyo: %d\nIce-cream Sandwich: %d\n",
                 orderCount[0],
                 orderCount[1],
                 orderCount[2]));
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.labels_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ((Spinner) findViewById(R.id.email_spinner)).setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(this, parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
     }
 }
