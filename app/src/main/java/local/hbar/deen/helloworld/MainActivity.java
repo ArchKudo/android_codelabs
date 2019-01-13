a733e524bc404c717810a8ee1a479b0b0f8af56f
ArchKudo <vgd.vyas17@gmail.com>
2019-01-14 00:36:42 +0530

    Cleanup files for 05.1

--- app/src/main/java/local/hbar/deen/helloworld/MainActivity.java
+++ app/src/main/java/local/hbar/deen/helloworld/MainActivity.java
@@ -1,158 +1,15 @@
 package local.hbar.deen.helloworld;
 
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.ContextMenu;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Setup Toolbar
-        setSupportActionBar(findViewById(R.id.toolbar));
-
-        // Setup Tabs
-        String[] tabNames = getResources().getStringArray(R.array.tab_names);
-        TabLayout tabLayout = findViewById(R.id.tab_layout);
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-        for (String tabName : tabNames) {
-            tabLayout.addTab(tabLayout.newTab().setText(tabName));
-        }
-
-        // Setup Tab content
-        final ViewPager productsViewPager = findViewById(R.id.products_view_pager);
-        final ProductsFragmentAdapter adapter = new ProductsFragmentAdapter(getSupportFragmentManager(), tabNames);
-        productsViewPager.setAdapter(adapter);
-
-        productsViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                productsViewPager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
-        // Setup App Drawer
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
-                this,
-                findViewById(R.id.drawer_layout),
-                findViewById(R.id.toolbar),
-                R.string.navigation_drawer_open,
-                R.string.navigation_drawer_close);
-        ((DrawerLayout) findViewById(R.id.drawer_layout)).addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
-        ((NavigationView) findViewById(R.id.navigation_view)).setNavigationItemSelectedListener(this);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        super.onOptionsItemSelected(item);
-        Toast.makeText(this, item.getTitle().toString(), Toast.LENGTH_SHORT).show();
-        return true;
-    }
-
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-        getMenuInflater().inflate(R.menu.menu_context, menu);
-    }
-
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-        super.onContextItemSelected(item);
-        Toast.makeText(this, item.getTitle().toString(), Toast.LENGTH_SHORT).show();
-        return true;
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-
-        // TODO: Simplify switch
-        // TODO: Actually add some functionality (like theme switcher?)
-        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
-        // Handle navigation view item clicks here.
-        switch (item.getItemId()) {
-            case R.id.nav_camera:
-                // Handle the camera import action (for now display a toast).
-                drawerLayout.closeDrawer(GravityCompat.START);
-                displayToast(getString(R.string.chose_camera));
-                return true;
-            case R.id.nav_gallery:
-                // Handle the gallery action (for now display a toast).
-                drawerLayout.closeDrawer(GravityCompat.START);
-                displayToast(getString(R.string.chose_gallery));
-                return true;
-            case R.id.nav_slideshow:
-                // Handle the slideshow action (for now display a toast).
-                drawerLayout.closeDrawer(GravityCompat.START);
-                displayToast(getString(R.string.chose_slideshow));
-                return true;
-            case R.id.nav_manage:
-                // Handle the tools action (for now display a toast).
-                drawerLayout.closeDrawer(GravityCompat.START);
-                displayToast(getString(R.string.chose_tools));
-                return true;
-            case R.id.nav_share:
-                // Handle the share action (for now display a toast).
-                drawerLayout.closeDrawer(GravityCompat.START);
-                displayToast(getString(R.string.chose_share));
-                return true;
-            case R.id.nav_send:
-                // Handle the send action (for now display a toast).
-                drawerLayout.closeDrawer(GravityCompat.START);
-                displayToast(getString(R.string.chose_send));
-                return true;
-            default:
-                return false;
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
-        if (drawerLayout != null) {
-            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-                drawerLayout.closeDrawer(GravityCompat.START);
-            } else {
-                super.onBackPressed();
-            }
-        }
-    }
-
-    public void displayToast(String message) {
-        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-    }
-
 }
