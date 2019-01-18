8eab2a3ce4d96c2d28d5799a34705ab01b465ef8
ArchKudo <vgd.vyas17@gmail.com>
2019-01-18 17:16:12 +0530

    A Player face appear in the Glide

---
+++ app/src/main/java/local/hbar/deen/helloworld/GlideAppModule.java
@@ -0,0 +1,8 @@
+package local.hbar.deen.helloworld;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class GlideAppModule extends AppGlideModule {
+}
--- app/src/main/java/local/hbar/deen/helloworld/PlayerRecyclerAdapter.java
+++ app/src/main/java/local/hbar/deen/helloworld/PlayerRecyclerAdapter.java
@@ -1,13 +1,29 @@
 package local.hbar.deen.helloworld;
 
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
+
 import java.util.ArrayList;
+import java.util.Locale;
+
 
 public class PlayerRecyclerAdapter extends
         RecyclerView.Adapter<PlayerRecyclerAdapter.PlayerViewHolder> {
@@ -34,8 +50,16 @@ public class PlayerRecyclerAdapter extends
         playerViewHolder.nationality.setText(player.get(5));
         playerViewHolder.club.setText(player.get(9));
         playerViewHolder.rating.setText(player.get(7));
-        playerViewHolder.age.setText(player.get(3));
-
+        playerViewHolder.age.setText(String.format(Locale.getDefault(), "Age: %s", player.get(3)));
+        GlideApp.with(playerViewHolder.name.getContext())
+                .load(player.get(4))
+                .error(GlideApp.with(playerViewHolder.name.getContext())
+                                .load("https://cdn.sofifa.org/players/4/notfound_0.png"))
+                .apply(RequestOptions.circleCropTransform())
+                .apply(new RequestOptions().override(200, 200))
+                
+                .fallback(R.mipmap.ic_launcher_foreground)
+                .into(playerViewHolder.player_img);
     }
 
     @Override
@@ -45,6 +69,7 @@ public class PlayerRecyclerAdapter extends
 
     class PlayerViewHolder extends RecyclerView.ViewHolder {
         TextView name, club, nationality, age, rating;
+        ImageView player_img;
 
         PlayerViewHolder(View view) {
             super(view);
@@ -53,6 +78,9 @@ public class PlayerRecyclerAdapter extends
             nationality = view.findViewById(R.id.text_nationality);
             age = view.findViewById(R.id.num_age);
             rating = view.findViewById(R.id.num_rating);
+            player_img = view.findViewById(R.id.img_player);
         }
     }
 }
+
+
