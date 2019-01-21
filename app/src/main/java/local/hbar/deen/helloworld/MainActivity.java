e963822aac3e2fd1c8d8c0a32d0e6073d7374b10
ArchKudo <vgd.vyas17@gmail.com>
2019-01-21 19:36:03 +0530

    m'save thot

--- app/src/main/java/local/hbar/deen/helloworld/MainActivity.java
+++ app/src/main/java/local/hbar/deen/helloworld/MainActivity.java
@@ -19,6 +19,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (savedInstanceState != null) {
+            ((TextView) findViewById(R.id.text_msg)).setText(String.format(Locale.getDefault(),
+                    "%s", savedInstanceState.getString("old_text")));
+        }
+
         findViewById(R.id.btn_woke).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,12 +36,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("old_text",
+                ((TextView) findViewById(R.id.text_msg)).getText().toString());
+    }
 }
 
 class SleepTask extends AsyncTask<Void, Void, String> {
 
-    WeakReference<TextView> mTextView;
+    private WeakReference<TextView> mTextView;
 
     SleepTask(TextView mTextView) {
         this.mTextView = new WeakReference<>(mTextView);
