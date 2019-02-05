b6da5da5b38ffc10b78342cd6f3d2c9ed0a9e3fc
ArchKudo <vgd.vyas17@gmail.com>
2019-02-05 17:48:57 +0530

    Added a simple shared prefs project / Rather than cloning the unnecessarily complicated app of tutorial 09.1

--- app/src/main/java/local/hbar/deen/helloworld/MainActivity.java
+++ app/src/main/java/local/hbar/deen/helloworld/MainActivity.java
@@ -1,7 +1,12 @@
 package local.hbar.deen.helloworld;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        int val = this.getPreferences(Context.MODE_PRIVATE).getInt("num", -1);
+
+        if (val != -1) {
+            ((EditText) findViewById(R.id.number_input)).setHint(String.format(Locale.getDefault(), "Previously saved: %d", val));
+        }
+
+        findViewById(R.id.save_btn).setOnClickListener(
+                (View v) -> {
+                    this.getPreferences(Context.MODE_PRIVATE)
+                            .edit()
+                            .putInt("num",
+                                    Integer.parseInt(((EditText) findViewById(R.id.number_input)).getText().toString()))
+                            .apply();
+                }
+        );
     }
 }
