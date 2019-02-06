ab5baf682593083e7cef07a1ae76612380bc8988
ArchKudo <vgd.vyas17@gmail.com>
2019-02-06 17:23:57 +0530

    Added a room database with a single instance and destructive migrations / Remove inner classes

--- app/src/main/java/local/hbar/deen/helloworld/MainActivity.java
+++ app/src/main/java/local/hbar/deen/helloworld/MainActivity.java
@@ -3,10 +3,14 @@ package local.hbar.deen.helloworld;
 import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.PrimaryKey;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
@@ -16,6 +20,54 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import java.util.List;
+
+@Dao
+interface WordDAO {
+
+    @Insert
+    void insert(Word word);
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAll();
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+}
+
+
+@Entity(tableName = "word_table")
+class Word {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String word;
+
+    Word(@NonNull String word) {
+        this.word = word;
+    }
+
+    String getWord() {
+        return this.word;
+    }
+}
+
+@Database(entities = {Word.class}, version = 1, exportSchema = false)
+abstract class WordRoomDatabase extends RoomDatabase {
+    private static WordRoomDatabase INSTANCE;
+
+    abstract WordDAO wordDAO();
+
+    WordRoomDatabase getDatatbase(final Context context) {
+        if (INSTANCE != null) {
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                    WordRoomDatabase.class, "word_database")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return INSTANCE;
+    }
+}
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,33 +88,5 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Entity(tableName = "word_table")
-    class Word {
 
-        @PrimaryKey
-        @NonNull
-        @ColumnInfo(name = "word")
-        private String word;
-
-        Word(@NonNull String word) {
-            this.word = word;
-        }
-
-        String getWord() {
-            return this.word;
-        }
-    }
-
-    @Dao
-    interface WordDAO {
-
-        @Insert
-        void insert(Word word);
-
-        @Query("SELECT * from word_table ORDER BY word ASC")
-        LiveData<List<Word>> getAll();
-
-        @Query("DELETE FROM word_table")
-        void deleteAll();
-    }
 }
